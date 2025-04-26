0ac84009507d64cc5be2e7439136bbbc6948295f
ddlaosant <ddlaosant@gmail.com>
2025-04-25 18:27:55 -0700

    add main.hpp

--- main.hpp
+++ main.hpp
@@ -7,14 +7,13 @@ int getNextPrime(int begin);
 int getPrevPrime(int end);
 bool isPrime(int n);
 
-
 void getTwoValues(int &begin, int &end)
 {
     do {
         cout << "Enter two integers (begin < end): ";
         cin >> begin >> end;
         if (begin >= end) {
-            
+            cout << "Invalid input. Make sure begin < end.\n";
         }
     } while (begin >= end);
 }
@@ -37,19 +36,19 @@ int getPrevPrime(int end)
     int num = end - 1;
     while (num > 1) {
         if (isPrime(num)) {
-           
+            return num;
         }
         num--;
     }
-    return -1; 
+    return -1;
 }
 
 
 bool isPrime(int n)
 {
-    if (n <= 1) return false
+    if (n <= 1) return false;
     for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i = 0) return false;
+        if (n % i == 0) return false;
     }
     return true;
 }
